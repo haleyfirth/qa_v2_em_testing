69b14c5f2f5fd6aea152bef7f2b6d6aaa98d7312
Haley <72286977+haleyfirth@users.noreply.github.com>
2021-01-09 19:43:23 -0700

    Update emv2.test.ts
    Final?

--- __tests__/emv2.test.ts
+++ __tests__/emv2.test.ts
@@ -41,21 +41,26 @@ describe("employee manager v2", () => {
     let resultList = await page.getEmployeeList();
     expect(originalList.length).toBeGreaterThanOrEqual(resultList.length);
   });
-  test("Can add and delete an employee", async () => {
-    let newEmployee = {
-      name: "Test Employee",
-      phone: 1234567890,
-      email: "test@email.com",
-      title: "test person",
-    };
-    await page.addEmployee(newEmployee);
-    let employee = await page.getCurrentEmployee();
-    expect(employee.name).toEqual(newEmployee.name);
-    expect(employee.phone).toEqual(newEmployee.phone);
-    expect(employee.email).toEqual(newEmployee.email);
-    expect(employee.title).toEqual(newEmployee.title);
-    await page.deleteEmployee("Test Employee");
-    let employeeList = await page.getEmployeeList();
-    expect(employeeList).not.toContain("Test Employee");
+  test("Screenshotting the 'screenshot' employees", async () => {
+    
+    await page.searchFor("Screenshot");
+    
+    await page.takeScreenshot("screenshots/screenshot");
   });
-});
+  employees.forEach((newEmployee) => {
+  
+    test(` add and delete an employee (newEmployee.name)`, async () => {
+      await page.addEmployee(newEmployee);
+      let employee = await page.getCurrentEmployee();
+      expect(employee.name).toEqual(newEmployee.name);
+      expect(employee.phone).toEqual(newEmployee.phone);
+      expect(employee.email).toEqual(newEmployee.email);
+      expect(employee.title).toEqual(newEmployee.title);
+      
+      await page.deleteEmployee(newEmployee.name);
+      let employeeList = await page.getEmployeeList();
+
+      expect(employeeList).not.toContain(newEmployee.name);
+    });
+  });
+}); 
